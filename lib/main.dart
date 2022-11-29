f12122318cf26643aca6c16e906aeac4a6a904e8
saci zakaria <sacideveloppeur@gmail.com>
2022-11-29 18:27:36 +0100

    MultiProvider

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:grocery_app_hadi_v1/const/theme_data.dart';
 import 'package:grocery_app_hadi_v1/screens/home_screen.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -17,10 +18,13 @@ class MyApp extends StatelessWidget {
   bool isDark = false;
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: Styles.themeData(true, context),
-      home: const HomeScreen(),
+    return MultiProvider(
+      providers: [],
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: Styles.themeData(true, context),
+        home: const HomeScreen(),
+      ),
     );
   }
 }
