32b44d43939ddd83b8e8f8382cb90edc16529815
saci zakaria <sacideveloppeur@gmail.com>
2022-11-29 17:14:57 +0100

    prefs.setBool(THEME_STATUS, value);

--- lib/screens/dark_theme_prefs.dart
+++ lib/screens/dark_theme_prefs.dart
@@ -1,8 +1,10 @@
 import 'package:shared_preferences/shared_preferences.dart';
 
 class DarkthemePrefs {
+  static const THEME_STATUS = "THEME_STATUS";
+
   setDarkTheme(bool value) async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
-    prefs.setBool('key', value);
+    prefs.setBool(THEME_STATUS, value);
   }
 }
