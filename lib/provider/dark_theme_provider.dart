92e805d45bfe8ffd159d29f84e95936904293840
saci zakaria <sacideveloppeur@gmail.com>
2022-11-29 18:15:59 +0100

    set setDarkTheme(bool value) {}

--- lib/provider/dark_theme_provider.dart
+++ lib/provider/dark_theme_provider.dart
@@ -3,4 +3,6 @@ import 'package:flutter/cupertino.dart';
 class DarkThemeProvider with ChangeNotifier {
   bool _darkTheme = false;
   bool get getDarkTheme => _darkTheme;
+
+  set setDarkTheme(bool value) {}
 }
