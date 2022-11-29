dc49f882bb85db9ad7abe01fe5a74cc0e274cd19
saci zakaria <sacideveloppeur@gmail.com>
2022-11-29 18:34:11 +0100

    DarkThemeProvider();

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:grocery_app_hadi_v1/const/theme_data.dart';
+import 'package:grocery_app_hadi_v1/provider/dark_theme_provider.dart';
 import 'package:grocery_app_hadi_v1/screens/home_screen.dart';
 import 'package:provider/provider.dart';
 
@@ -15,6 +16,7 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
+  DarkThemeProvider themeChangeProvider = DarkThemeProvider();
   void getCurrentAppThem() {}
 
   @override
