9963ef17a3f2171f2993098913abd21bf130705e
saci zakaria <sacideveloppeur@gmail.com>
2022-11-30 10:29:50 +0100

    BottomBarScreen

---
+++ lib/screens/btm_bar.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class BottomBarScreen extends StatefulWidget {
+  const BottomBarScreen({super.key});
+
+  @override
+  State<BottomBarScreen> createState() => _BottomBarScreenState();
+}
+
+class _BottomBarScreenState extends State<BottomBarScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
