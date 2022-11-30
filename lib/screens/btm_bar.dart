28bf6e9dd1e9daeafe957b077b1930671c4d0b42
saci zakaria <sacideveloppeur@gmail.com>
2022-11-30 10:45:47 +0100

    final List<Map<String, dynamic>> _pages

--- lib/screens/btm_bar.dart
+++ lib/screens/btm_bar.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:grocery_app_hadi_v1/screens/cart.dart';
+import 'package:grocery_app_hadi_v1/screens/categories.dart';
+import 'package:grocery_app_hadi_v1/screens/home_screen.dart';
+import 'package:grocery_app_hadi_v1/screens/user.dart';
 
 class BottomBarScreen extends StatefulWidget {
   const BottomBarScreen({super.key});
@@ -9,6 +13,13 @@ class BottomBarScreen extends StatefulWidget {
 }
 
 class _BottomBarScreenState extends State<BottomBarScreen> {
+  final List<Map<String, dynamic>> _pages = [
+    {'page': const HomeScreen(), 'title': 'Home Screen'},
+    {'page': const CategoriesScreen(), 'title': 'Categories Screen'},
+    {'page': const CartScreen(), 'title': 'Cart Screen'},
+    {'page': const UserScreen(), 'title': 'user Screen'},
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Container();
