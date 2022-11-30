74302647baf25cec68b0b314987eb2257c4aebd9
saci zakaria <sacideveloppeur@gmail.com>
2022-11-30 09:31:52 +0100

    Provider.of(context);

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
+  //
+
   @override
   Widget build(BuildContext context) {
+    Provider.of(context);
     return Scaffold(
       body: Center(
         child: SwitchListTile(
