3860c3cd096fd84366f187dcfe588e4f5ebb948e
saci zakaria <sacideveloppeur@gmail.com>
2022-12-04 18:22:23 +0100

    bool isTitle;

--- lib/widgets/text_widget.dart
+++ lib/widgets/text_widget.dart
@@ -1,16 +1,18 @@
 import 'package:flutter/material.dart';
 
 class TextWidget extends StatelessWidget {
-  const TextWidget({
+  TextWidget({
     Key? key,
     required this.text,
     required this.color,
     required this.textSize,
+    this.isTitle = false,
   }) : super(key: key);
 
   final String text;
   final Color color;
   final double textSize;
+  bool isTitle;
 
   @override
   Widget build(BuildContext context) {
