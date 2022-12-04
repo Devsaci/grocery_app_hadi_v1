ab866372d81cd97af95220ed78e0d807124b0444
saci zakaria <sacideveloppeur@gmail.com>
2022-12-04 18:10:58 +0100

    required this.text,

--- lib/widgets/text_widget.dart
+++ lib/widgets/text_widget.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
 
 class TextWidget extends StatelessWidget {
-  const TextWidget({super.key});
+  const TextWidget({
+    Key? key,
+    required this.text,
+  }) : super(key: key);
+
+  final String text;
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Text(
+      text,
+      style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+    );
   }
 }
