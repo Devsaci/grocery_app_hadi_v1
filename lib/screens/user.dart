44c6ccb57558a30552fc78ea09d91ab5572ceac9
saci zakaria <sacideveloppeur@gmail.com>
2022-12-08 19:07:50 +0100

    Fix bug Update Code

--- lib/screens/user.dart
+++ lib/screens/user.dart
@@ -6,7 +6,7 @@ import 'package:grocery_app_hadi_v1/widgets/text_widget.dart';
 import 'package:provider/provider.dart';
 
 class UserScreen extends StatefulWidget {
-  const UserScreen({super.key});
+  const UserScreen({Key? key}) : super(key: key);
 
   @override
   State<UserScreen> createState() => _UserScreenState();
@@ -15,7 +15,6 @@ class UserScreen extends StatefulWidget {
 class _UserScreenState extends State<UserScreen> {
   final TextEditingController _addressTextController =
       TextEditingController(text: "");
-
   @override
   void dispose() {
     _addressTextController.dispose();
@@ -25,156 +24,134 @@ class _UserScreenState extends State<UserScreen> {
   @override
   Widget build(BuildContext context) {
     final themeState = Provider.of<DarkThemeProvider>(context);
-
     final Color color = themeState.getDarkTheme ? Colors.white : Colors.black;
     return Scaffold(
-      body: Center(
-        child: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                RichText(
-                  text: TextSpan(
-                    text: 'Hi,  ',
-                    style: const TextStyle(
-                      color: Colors.cyan,
-                      fontSize: 27,
-                      fontWeight: FontWeight.bold,
-                    ),
-                    children: <TextSpan>[
-                      TextSpan(
-                          text: 'MyName',
-                          style: TextStyle(
-                            color: color,
-                            fontSize: 25,
-                            fontWeight: FontWeight.w600,
-                          ),
-                          recognizer: TapGestureRecognizer()
-                            ..onTap = () {
-                              print('My Name Is toto la musique');
-                            }),
-                    ],
+        body: Center(
+      child: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const SizedBox(
+                height: 15,
+              ),
+              RichText(
+                text: TextSpan(
+                  text: 'Hi,  ',
+                  style: const TextStyle(
+                    color: Colors.cyan,
+                    fontSize: 27,
+                    fontWeight: FontWeight.bold,
                   ),
+                  children: <TextSpan>[
+                    TextSpan(
+                        text: 'MyName',
+                        style: TextStyle(
+                          color: color,
+                          fontSize: 25,
+                          fontWeight: FontWeight.w600,
+                        ),
+                        recognizer: TapGestureRecognizer()
+                          ..onTap = () {
+                            print('My name is pressed');
+                          }),
+                  ],
                 ),
-                const SizedBox(
-                  height: 5,
-                ),
-                TextWidget(
-                  text: "saci@gmail.com",
+              ),
+              const SizedBox(
+                height: 5,
+              ),
+              TextWidget(
+                text: 'Email@email.com',
+                color: color,
+                textSize: 18,
+                // isTitle: true,
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              const Divider(
+                thickness: 2,
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              _listTiles(
+                title: 'Address 2',
+                subtitle: 'My subtitle',
+                icon: IconlyLight.profile,
+                onPressed: () async {
+                  await _showAddressDialog();
+                },
+                color: color,
+              ),
+              _listTiles(
+                title: 'Orders',
+                icon: IconlyLight.bag,
+                onPressed: () {},
+                color: color,
+              ),
+              _listTiles(
+                title: 'Wishlist',
+                icon: IconlyLight.heart,
+                onPressed: () {},
+                color: color,
+              ),
+              _listTiles(
+                title: 'Viewed',
+                icon: IconlyLight.show,
+                onPressed: () {},
+                color: color,
+              ),
+              _listTiles(
+                title: 'Forget password',
+                icon: IconlyLight.unlock,
+                onPressed: () {},
+                color: color,
+              ),
+              SwitchListTile(
+                title: TextWidget(
+                  text: themeState.getDarkTheme ? 'Dark mode' : 'Light mode',
                   color: color,
                   textSize: 18,
-                  isTitle: true,
+                  // isTitle: true,
                 ),
-                const SizedBox(
-                  height: 15,
-                ),
-                const Divider(
-                  thickness: 5,
-                ),
-                const SizedBox(
-                  height: 15,
-                ),
-                // _listTiles(
-                //   color: color,
-                //   title: 'Address',
-                //   subtitle: 'My Address',
-                //   icon: IconlyLight.profile,
-                //   onPressed: () async {
-                //     await showDialog(
-                //         context: context,
-                //         builder: (context) {
-                //           return AlertDialog(
-                //             title: const Text("Update"),
-                //             content: TextField(
-                //               onChanged: (value) {},
-                //               controller: _addressTextController,
-                //               maxLines: 5,
-                //               decoration: const InputDecoration(
-                //                   hintText: "Your address"),
-                //             ),
-                //           );
-                //         });
-                //   },
-                // ),
-                _listTiles(
-                  title: 'Address 2',
-                  subtitle: 'My subtitle',
-                  icon: IconlyLight.profile,
-                  onPressed: () async {
-                    await _showAddressDialog();
-                  },
-                  color: color,
-                ),
-                _listTiles(
-                  title: 'Orders',
-                  icon: IconlyLight.bag,
-                  onPressed: () {},
-                  color: color,
-                ),
-                _listTiles(
-                  title: 'Wishlist',
-                  icon: IconlyLight.heart,
-                  onPressed: () {},
-                  color: color,
-                ),
-                _listTiles(
-                  title: 'Viewed',
-                  icon: IconlyLight.show,
-                  onPressed: () {},
-                  color: color,
-                ),
-                _listTiles(
-                  title: 'Forget password',
-                  icon: IconlyLight.unlock,
-                  onPressed: () {},
-                  color: color,
-                ),
-                _listTiles(
-                  title: 'Logout',
-                  icon: IconlyLight.logout,
-                  onPressed: () {},
-                  color: color,
-                ),
-                SwitchListTile(
-                  secondary: Icon(themeState.getDarkTheme
-                      ? Icons.dark_mode_outlined
-                      : Icons.light_mode_outlined),
-                  title: TextWidget(
-                    text:
-                        themeState.getDarkTheme ? "Dark Theme" : "Light Theme",
-                    color: color,
-                    textSize: 18,
-                    isTitle: true,
-                  ),
-                  value: themeState.getDarkTheme,
-                  onChanged: (bool value) {
-                    setState(() {
-                      themeState.setDarkTheme = value;
-                    });
-                  },
-                ),
-              ],
-            ),
+                secondary: Icon(themeState.getDarkTheme
+                    ? Icons.dark_mode_outlined
+                    : Icons.light_mode_outlined),
+                onChanged: (bool value) {
+                  setState(() {
+                    themeState.setDarkTheme = value;
+                  });
+                },
+                value: themeState.getDarkTheme,
+              ),
+              _listTiles(
+                title: 'Logout',
+                icon: IconlyLight.logout,
+                onPressed: () {},
+                color: color,
+              ),
+              // listTileAsRow(),
+            ],
           ),
         ),
       ),
-    );
+    ));
   }
 
   Future<void> _showAddressDialog() async {
-    await showDialog<void>(
+    await showDialog(
         context: context,
-        builder: (BuildContext context) {
+        builder: (context) {
           return AlertDialog(
             title: const Text('Update'),
             content: TextField(
-              onChanged: (value) {
-                print(
-                    '_addressTextController.text ${_addressTextController.text}');
-              },
+              // onChanged: (value) {
+              //   print('_addressTextController.text ${_addressTextController.text}');
+              // },
               controller: _addressTextController,
               maxLines: 5,
               decoration: const InputDecoration(hintText: "Your address"),
@@ -201,22 +178,18 @@ class _UserScreenState extends State<UserScreen> {
         text: title,
         color: color,
         textSize: 22,
-        isTitle: true,
+        // isTitle: true,
       ),
-      // subtitle: Text(subtitle ?? ""),
       subtitle: TextWidget(
         text: subtitle == null ? "" : subtitle,
         color: color,
-        textSize: 10,
+        textSize: 18,
       ),
       leading: Icon(icon),
-      trailing: const Icon(IconlyLight.arrowRight),
+      trailing: const Icon(IconlyLight.arrowRight2),
       onTap: () {
-        onPressed() {}
+        onPressed();
       },
     );
   }
 }
-
-// TextWidget(
-//     {required String text, required Color color, required int textSize}) {}
