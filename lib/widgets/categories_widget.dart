f7169e4ccb0c73b3c7e82aaf2237966c4eb75840
saci zakaria <sacideveloppeur@gmail.com>
2022-12-11 18:21:11 +0100

    width: 2,

--- lib/widgets/categories_widget.dart
+++ lib/widgets/categories_widget.dart
@@ -7,9 +7,13 @@ class CategoriesWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
-          color: Colors.red.withOpacity(0.1),
-          borderRadius: BorderRadius.circular(16),
-          border: Border.all()),
+        color: Colors.red.withOpacity(0.1),
+        borderRadius: BorderRadius.circular(16),
+        border: Border.all(
+          color: Colors.red.withOpacity(0.7),
+          width: 2,
+        ),
+      ),
     );
   }
 }
