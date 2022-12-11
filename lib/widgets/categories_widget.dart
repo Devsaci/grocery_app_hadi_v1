912a5dbf1aec8f09ddc1ca28ad285dbb9cb1006e
saci zakaria <sacideveloppeur@gmail.com>
2022-12-11 18:10:52 +0100

    BoxDecoration

--- lib/widgets/categories_widget.dart
+++ lib/widgets/categories_widget.dart
@@ -1,11 +1,12 @@
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/material.dart';
 
 class CategoriesWidget extends StatelessWidget {
   const CategoriesWidget({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      decoration: BoxDecoration(),
+    );
   }
 }
