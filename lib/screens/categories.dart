bd2551b347ffac85cd65eac830fd07e1f14bf283
saci zakaria <sacideveloppeur@gmail.com>
2022-12-13 15:55:03 +0100

    List<Color> gridColors

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -6,6 +6,15 @@ import 'package:grocery_app_hadi_v1/widgets/text_widget.dart';
 class CategoriesScreen extends StatelessWidget {
   CategoriesScreen({Key? key}) : super(key: key);
 
+  List<Color> gridColors = [
+    const Color(0xff53B175),
+    const Color(0xffF8A44C),
+    const Color(0xffF7A593),
+    const Color(0xffD3B0E0),
+    const Color(0xffFDE598),
+    const Color(0xffB7DFF5),
+  ];
+
   List<Map<String, dynamic>> catInfo = [
     {
       'imgPath': 'assets/images/cat/fruits.png',
