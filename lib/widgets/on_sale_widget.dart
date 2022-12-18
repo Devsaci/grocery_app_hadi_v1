eb7822ff033da1603067076921adf77245d08a11
saci zakaria <sacideveloppeur@gmail.com>
2022-12-18 19:54:30 +0100

    Utils(context).getScreenSize;

--- lib/widgets/on_sale_widget.dart
+++ lib/widgets/on_sale_widget.dart
@@ -1,6 +1,7 @@
 //23. Implement the sale widget
 
 import 'package:flutter/material.dart';
+import 'package:grocery_app_hadi_v1/services/utils.dart';
 
 class OnSaleWidget extends StatefulWidget {
   const OnSaleWidget({Key? key}) : super(key: key);
@@ -10,8 +11,10 @@ class OnSaleWidget extends StatefulWidget {
 }
 
 class _OnSaleWidgetState extends State<OnSaleWidget> {
+  // Utils(context).getScreenSize;
   @override
   Widget build(BuildContext context) {
+    Utils(context).getScreenSize;
     return Container();
   }
 }
