1abea82bf2d29faa10540e30f19d7fffc8b43ac5
saci zakaria <sacideveloppeur@gmail.com>
2022-12-18 19:48:16 +0100

    class OnSaleWidget extends StatefulWidget

--- lib/widgets/on_sale_widget.dart
+++ lib/widgets/on_sale_widget.dart
@@ -1,1 +1,17 @@
-//23. Implement the sale widget 
+//23. Implement the sale widget
+
+import 'package:flutter/material.dart';
+
+class OnSaleWidget extends StatefulWidget {
+  const OnSaleWidget({super.key});
+
+  @override
+  State<OnSaleWidget> createState() => _OnSaleWidgetState();
+}
+
+class _OnSaleWidgetState extends State<OnSaleWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
