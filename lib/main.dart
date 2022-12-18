f90965f72b50d00ea1dca70014816f31ced49f8f
saci zakaria <sacideveloppeur@gmail.com>
2022-12-18 21:30:44 +0100

    home: BottomBarScreen(),

--- lib/main.dart
+++ lib/main.dart
@@ -52,7 +52,7 @@ class _MyAppState extends State<MyApp> {
         return MaterialApp(
           title: 'Flutter Demo',
           theme: Styles.themeData(themeProvider.getDarkTheme, context),
-          home: HomeScreen(),
+          home: BottomBarScreen(),
           // home: const DialogExample(),
         );
       }),
