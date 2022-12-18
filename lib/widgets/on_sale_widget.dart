a99c9c193da738a4d3a6a79e8b9d9575b32bfa8b
saci zakaria <sacideveloppeur@gmail.com>
2022-12-18 19:57:24 +0100

    final theme =

--- lib/widgets/on_sale_widget.dart
+++ lib/widgets/on_sale_widget.dart
@@ -13,7 +13,7 @@ class OnSaleWidget extends StatefulWidget {
 class _OnSaleWidgetState extends State<OnSaleWidget> {
   @override
   Widget build(BuildContext context) {
-    Utils(context).getTheme;
+    final theme = Utils(context).getTheme;
     Size size = Utils(context).getScreenSize;
     return Container();
   }
