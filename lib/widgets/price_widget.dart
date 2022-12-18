101334bc3964f065798a06150373b03bfd65a747
saci zakaria <sacideveloppeur@gmail.com>
2022-12-18 22:26:16 +0100

    const PriceWidget(),

---
+++ lib/widgets/price_widget.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class PriceWidget extends StatelessWidget {
+  const PriceWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
