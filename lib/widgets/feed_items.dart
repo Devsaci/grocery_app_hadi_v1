5b464a1d58eeb6216e5f7f4b1ccfa58b3d132d17
saci zakaria <sacideveloppeur@gmail.com>
2023-01-01 21:59:51 +0100

    27. Implement the feeds widget and add it to the Home screen

--- lib/widgets/feed_items.dart
+++ lib/widgets/feed_items.dart
@@ -1,2 +1,18 @@
 // 27. Implement the feeds widget and add it to the Home screen
 
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/material.dart';
+
+class FeedsWidget extends StatefulWidget {
+  const FeedsWidget({super.key});
+
+  @override
+  State<FeedsWidget> createState() => _FeedsWidgetState();
+}
+
+class _FeedsWidgetState extends State<FeedsWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
