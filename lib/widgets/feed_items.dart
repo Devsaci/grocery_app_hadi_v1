4dafb93af0702795534b4abd38e270154406a73b
saci zakaria <sacideveloppeur@gmail.com>
2023-01-02 16:27:37 +0100

    Row / children / TextWidget

--- lib/widgets/feed_items.dart
+++ lib/widgets/feed_items.dart
@@ -3,6 +3,7 @@
 import 'package:fancy_shimmer_image/fancy_shimmer_image.dart';
 import 'package:flutter/material.dart';
 import 'package:grocery_app_hadi_v1/services/utils.dart';
+import 'package:grocery_app_hadi_v1/widgets/text_widget.dart';
 
 class FeedsWidget extends StatefulWidget {
   const FeedsWidget({super.key});
@@ -14,6 +15,7 @@ class FeedsWidget extends StatefulWidget {
 class _FeedsWidgetState extends State<FeedsWidget> {
   @override
   Widget build(BuildContext context) {
+    final Color color = Utils(context).color;
     Size size = Utils(context).getScreenSize;
     return Padding(
       padding: const EdgeInsets.all(8.0),
@@ -30,6 +32,16 @@ class _FeedsWidgetState extends State<FeedsWidget> {
                 height: size.width * 0.21,
                 width: size.width * 0.2,
                 boxFit: BoxFit.fill,
+              ),
+              Row(
+                children: [
+                  TextWidget(
+                    text: 'Title',
+                    color: color,
+                    textSize: 20,
+                    isTitle: true,
+                  ),
+                ],
               )
             ],
           ),
