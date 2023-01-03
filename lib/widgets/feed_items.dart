a660d0dab11b3b54fb8025397958a93f37f626b1
saci zakaria <sacideveloppeur@gmail.com>
2023-01-03 16:01:25 +0100

    dispose()

--- lib/widgets/feed_items.dart
+++ lib/widgets/feed_items.dart
@@ -24,6 +24,12 @@ class _FeedsWidgetState extends State<FeedsWidget> {
   }
 
   @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     final Color color = Utils(context).color;
     Size size = Utils(context).getScreenSize;
