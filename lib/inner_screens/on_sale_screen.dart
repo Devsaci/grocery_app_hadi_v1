521289a60f3ed9b0bf0075bedea6a52f0c7a84a0
saci zakaria <sacideveloppeur@gmail.com>
2023-01-06 09:42:19 +0100

    class OnSaleScreen

--- lib/inner_screens/on_sale_screen.dart
+++ lib/inner_screens/on_sale_screen.dart
@@ -1,1 +1,18 @@
 //30. Implement the sale screen
+
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/material.dart';
+
+class OnSaleScreen extends StatefulWidget {
+  const OnSaleScreen({super.key});
+
+  @override
+  State<OnSaleScreen> createState() => _OnSaleScreenState();
+}
+
+class _OnSaleScreenState extends State<OnSaleScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
