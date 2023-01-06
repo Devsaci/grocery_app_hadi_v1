7ec3143477ba28ebdbf978bc91714ac875649c89
saci zakaria <sacideveloppeur@gmail.com>
2023-01-06 09:53:31 +0100

    OnSaleWidget()

--- lib/inner_screens/on_sale_screen.dart
+++ lib/inner_screens/on_sale_screen.dart
@@ -4,6 +4,7 @@ import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/material.dart';
 import 'package:grocery_app_hadi_v1/services/utils.dart';
 import 'package:grocery_app_hadi_v1/widgets/feed_items.dart';
+import 'package:grocery_app_hadi_v1/widgets/on_sale_widget.dart';
 
 class OnSaleScreen extends StatefulWidget {
   const OnSaleScreen({super.key});
@@ -24,7 +25,7 @@ class _OnSaleScreenState extends State<OnSaleScreen> {
         crossAxisCount: 2,
         padding: EdgeInsets.zero,
         childAspectRatio: size.width / (size.height * 0.59),
-        children: List.generate(4, (index) => const FeedsWidget()),
+        children: List.generate(4, (index) => const OnSaleWidget()),
       ),
     );
   }
